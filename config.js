2a79d625fda8e5441f68b2f9484b5462754e66d4
Mister428 <mangawords25@gmail.com>
2026-03-29 16:32:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     prefix: '!', // Préfixe des commandes
-    ownerNumber: '2250700000000', // Ton numéro WhatsApp (sans le + et sans le code pays si Baileys le gère automatiquement, ex: '2250700000000')
+    ownerNumber: '224612908366', // Ton numéro WhatsApp (sans le + et sans le code pays si Baileys le gère automatiquement, ex: '2250700000000')
     botName: 'Atsushi-Bot',
     sessionName: 'atsushi-session', // Nom de la session Baileys
     dbFile: './db.json', // Fichier pour la base de données simple
