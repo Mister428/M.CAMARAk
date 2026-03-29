c0daee0a666258593c3e77879ec31af26b5db643
Mister428 <mangawords25@gmail.com>
2026-03-29 16:47:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
     prefix: '!', // Préfixe des commandes
     ownerNumber: '224612908366', // Ton numéro WhatsApp (sans le + et sans le code pays si Baileys le gère automatiquement, ex: '2250700000000')
     botName: 'Atsushi-Bot',
-    sessionName: 'atsushi-session', // Nom de la session Baileys
+    sessionNames: ['session1', 'session2'], // <-- NOUVEAU: Noms des dossiers pour chaque session
     dbFile: './db.json', // Fichier pour la base de données simple
-    // Tu peux ajouter d'autres configurations ici si nécessaire
+    apiPort: process.env.PORT || 3000, // Port pour l'API web
+    apiKey: 'YOUR_SUPER_SECRET_API_KEY', // <-- NOUVEAU: Clé API pour le dashboard
 };
